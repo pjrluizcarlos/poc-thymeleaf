640b6e20354bf32f76c5f371aa9b1e1267dfbc4a
Luiz Carlos Pinto Junior <luiz.ilegra@terceiros.zenvia.com>
2019-09-16 19:22:53 -0300

    Initial Structure

---
+++ src/main/java/br/com/luizcarlospjr/springbootthymeleaf/WelcomeController.java
@@ -0,0 +1,35 @@
+package br.com.luizcarlospjr.springbootthymeleaf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class WelcomeController {
+
+    @Value("${welcome.message}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+    @GetMapping("/teste")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+
+        return "welcome";
+    }
+
+    @GetMapping("/hello")
+    public String mainWithParam(@RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
+        model.addAttribute("message", name);
+
+        return "welcome";
+    }
+
+}
